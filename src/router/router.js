90a6568b4901b925f2a805621050a570ea97a4a9
btea <2356281422@qq.com>
2019-09-21 18:32:36 +0800

    chunkName

--- src/router/router.js
+++ src/router/router.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-const Index = () => import('@/components/Index');
-const A = () => import('@/components/A');
-const B = () => import('@/components/B');
+// 动态引入的模块能动态加载，分文件打包
+// 注释里的模块名则是打包时生成的代码块的名字
+const Index = () => import(/* webpackChunkName: "Index" */ '@/components/Index');
+const A = () => import(/* webpackChunkName: "A"*/ '@/components/A');
+const B = () => import(/* webpackChunkName: "B"*/ '@/components/B');
 
 const routes = [
     {
